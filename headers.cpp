377ff57c8e78fea24cd0319fb38ac982168ed70c
Steve <steve_weng@hotmail.ca>
2014-11-15 16:48:59 -0500

    initial commit

---
+++ headers.cpp
@@ -0,0 +1,17 @@
+#ifndef __HEADERS_H__
+#define __HEADERS_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//SFML material
+#include <SFML/System.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+
+#include <iostream>
+#include <cassert>
+
+
+#endif
